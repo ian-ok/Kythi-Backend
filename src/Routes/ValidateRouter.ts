8d6124eabf57f6f38b3c514d2edad20a1e0a888e
Neko <78627265+Nekoyre@users.noreply.github.com>
2021-12-20 21:06:05 -0500

    feat: Endpoint to validate register parameters

---
+++ src/Routes/ValidateRouter.ts
@@ -0,0 +1,40 @@
+import Joi from 'joi';
+import type {FastifyInstance} from 'fastify';
+import {sendReply} from '../Utility';
+
+interface registerParams {
+  username: string;
+  email: string;
+  password: string;
+  inviteCode: string;
+}
+
+/**
+  The Router
+  @param {FastifyInstance} fastify
+*/
+export default async function BaseRouter(fastify: FastifyInstance) {
+  fastify.get<{Querystring: registerParams}>('/register', async (request, reply) => {
+    const registerSchema = Joi.object().keys({
+      username: Joi.string()
+          .required()
+          .min(4)
+          .max(24)
+          .pattern(/^[a-zA-Z0-9_]+$/),
+      email: Joi.string().required().email().lowercase(),
+      password: Joi.string().min(10).max(100).required(),
+      inviteCode: Joi.string().required(),
+    });
+
+    try {
+      await registerSchema.validateAsync(request.query);
+
+      sendReply(reply, 200, 'Valid Parameters', {success: true});
+    } catch (e: unknown) {
+      const error = e as Joi.ValidationError;
+      sendReply(reply, 400, error.details[0].message, {success: false});
+    }
+  });
+}
+
+export const autoPrefix = '/validate';
