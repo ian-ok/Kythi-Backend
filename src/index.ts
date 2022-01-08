09d7f129cce892991d0186dde5808d5926377c70
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-01-08 17:33:48 -0500

    feat: ESLint

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,13 @@
-import "dotenv/config";
-import fastify from "fastify";
+import 'dotenv/config';
+import fastify from 'fastify';
 
 const server = fastify();
 
-server.get("/", async () => {
-  return { statusCode: 200, message: "Hello World!" };
+server.get('/', async () => {
+  return {statusCode: 200, message: 'Hello World!'};
 });
 
-server.listen(process.env.PORT, "0.0.0.0", (err) => {
+server.listen(process.env.PORT, '0.0.0.0', (err) => {
   if (err) throw err;
 
   console.log(`Listening on http://127.0.0.1:${process.env.PORT}/`);
