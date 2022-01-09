85bc6aafd69606b7c214cfeccbc74c62902d1ba7
Kurp <78627265+Kurpp@users.noreply.github.com>
2022-01-09 13:38:23 -0500

    feat: Fastify Config, Ratelimit, Helmet, Cors

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,28 @@
 import 'dotenv/config';
 import {join} from 'path';
 import fastify from 'fastify';
+import fastifyCors from 'fastify-cors';
+import fastifyHelmet from 'fastify-helmet';
 import {PrismaClient} from '@prisma/client';
 import fastifyAutoload from 'fastify-autoload';
+import fastifyRateLimit from 'fastify-rate-limit';
 
-const server = fastify();
+const server = fastify({
+  trustProxy: true,
+});
 server.prisma = new PrismaClient();
+
+server.register(fastifyHelmet);
+
+server.register(fastifyCors, {
+  origin: ['https://kythi.com', /^http:\/\/localhost:\d+$/],
+  credentials: true,
+});
+
+server.register(fastifyRateLimit, {
+  timeWindow: 1000 * 60,
+  max: 20,
+});
 
 server.register(fastifyAutoload, {
   dir: join(__dirname, 'Routes'),
